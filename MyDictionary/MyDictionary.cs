e299d47b14b789df6ec17b4a9f6456e6e8753397
Nurhayat <69644512+nurhayatketenci@users.noreply.github.com>
2021-02-14 00:56:33 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    public class MyDictionary<T>
+    {
+        T[] arr;
+        T[] tempArray;
+
+        public MyDictionary()
+        {
+            arr = new T[0];
+        }
+
+        public void Add(T item)
+        {
+            tempArray = arr;
+            arr = new T[arr.Length + 1];
+
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                arr[i] = tempArray[i];
+            }
+            arr[arr.Length - 1] = item;
+
+        }
+
+        public int Count
+        {
+            get { return arr.Length; }
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string> names = new MyDictionary<string>();
+            names.Add("Nur");
+            names.Add("İrem");
+            names.Add("Didem");
+            names.Add("Eda");
+
+
+            Console.WriteLine("MyDictionary " + names.Count + " elemanlıdır.");
+        }
+    }
+}
